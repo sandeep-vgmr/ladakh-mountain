063a827be4b3a4efd9bfb7543dbc2a4f4ebffd4c
Bhupendra Singh <bhupendra.singh@sublimis.tech>
2023-08-04 15:32:17 +0530

    completed all page

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         delay: 50,
         duration: 800,
         once: true,
-        offset:0
+        offset: 0
     });
 
     $(window).scroll(function () {
@@ -33,5 +33,53 @@ $(document).ready(function () {
             scrollTop: 0
         }, 1500, 'easeInOutExpo');
         return false;
+    });
+
+    // mobile form funtionality 
+    $('.headingForm').click(function () {
+        $('.mobile-form').toggleClass("openUp");
+        $('.downArrow').toggleClass("rotateArrow");
+    });
+
+    // onclick video play and pause functionality 
+    // const video = document.getElementById("video");
+    // const circlePlayButton = document.getElementById("play-button");
+
+    // function togglePlay() {
+    //     if (video.paused || video.ended) {
+    //         video.play();
+    //     } else {
+    //         video.pause();
+    //     }
+    // }
+
+    // circlePlayButton.addEventListener("click", togglePlay);
+    // video.addEventListener("playing", function () {
+    //     circlePlayButton.style.opacity = 0;
+    // });
+    // video.addEventListener("pause", function () {
+    //     circlePlayButton.style.opacity = 1;
+    // });
+
+
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+      mainClass: 'mfp-with-zoom', 
+      gallery:{
+                enabled:true
+            },
+    
+      zoom: {
+        enabled: true, 
+    
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+    
+        opener: function(openerElement) {
+    
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+    
     });
 });
